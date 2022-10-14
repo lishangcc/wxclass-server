6934f2076b23e01de51df8e5f822da5e6522e7b1
DSLS <8916460+lishangcc@user.noreply.gitee.com>
2022-10-14 13:20:22 +0800

    first commit

---
+++ src/test/java/com/example/wxclassserver/WxclassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wxclassserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxclassServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
