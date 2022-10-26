fb9c10d4012d0a88729a9103451d9504c6d604c9
liubowen <723743690@qq.com>
2022-10-26 11:57:23 +0800

    chapter

---
+++ src/main/java/com/example/wxclassserver/controller/ChapterController.java
@@ -0,0 +1,80 @@
+package com.example.wxclassserver.controller;
+
+
+import com.example.wxclassserver.model.Chapter;
+import com.example.wxclassserver.model.Pages;
+import com.example.wxclassserver.model.Response;
+import com.example.wxclassserver.service.ChapterService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * description: ChapterController <br>
+ * date: 2022-10-13 15:00 <br>
+ * author: DaTao <br>
+ * version: 1.0 <br>
+ */
+@RestController
+@RequestMapping("/chapter")
+public class ChapterController {
+
+    @Resource
+    private ChapterService chapterService;
+
+    //物理路径
+    @Value("${file.upload.path}")
+    private String filePath;
+
+    /**显示虚拟地址*/
+    @Value("${file.upload.relative}")
+    private String fileRelativePath;
+
+
+    @GetMapping("/getPage")
+    public Pages<Chapter> getPage(Integer pageIndex, Integer pageSize, Integer courseId, String chapterName){
+
+        Pages<Chapter> pagers = chapterService.getPagers(pageIndex, pageSize,courseId,chapterName);
+        return pagers;
+    }
+
+
+    public Response add(Chapter chapter){
+        chapterService.add(chapter);
+
+        return new Response(200,"success");
+    }
+
+
+    /**
+     * 上传课程封面
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value="/upload",method = RequestMethod.POST)
+    public Response upload(MultipartFile file) throws IOException {
+
+        //获取文件原名
+        String fileName = file.getOriginalFilename();
+        //创建要上传的文件对象
+        File newFile = new File(filePath,fileName);
+        //将文件copy到目标地址
+        file.transferTo(newFile);
+
+        String filePath = fileRelativePath.replace("*","")+fileName;
+
+        return new Response(200,"success",filePath);
+    }
+
+}
--- src/main/java/com/example/wxclassserver/mapper/ChapterMapper.java
+++ src/main/java/com/example/wxclassserver/mapper/ChapterMapper.java
@@ -1,36 +1,48 @@
 package com.example.wxclassserver.mapper;
 
 import com.example.wxclassserver.model.Chapter;
+
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
+/**
+ * description: ChapterMapper <br>
+ * date: 2022-10-13 15:05 <br>
+ * author: DaTao <br>
+ * version: 1.0 <br>
+ */
 @Mapper
 public interface ChapterMapper {
 
 
     @Select({"<script> SELECT c1.id,c1.course_id,c2.name courseName,c1.name,c1.info,c1.video,c1.status" +
-            " FROM chapter c1 JOIN course c2 ON c1.course_id=c2.id where c1.course_id = #{id}"+
+            " FROM chapter c1 JOIN course c2 ON c1.course_id=c2.id where c1.course_id = #{courseId}"+
             "<where>" +
             "<if test='chapterName != null'> and c1.name like '%${chapterName}%' </if> " +
             "</where>" +
             "LIMIT #{recordStart},#{pageSize} "+
             "</script>"})
-    List<Chapter> queryCourse(Integer recordStart, Integer pageSize, Integer courseId, String chapterName);
+    List<Chapter> queryCourse(@Param("recordStart") Integer recordStart,
+                              @Param("pageSize") Integer pageSize,
+                              @Param("courseId") Integer courseId,
+                              @Param("chapterName") String chapterName);
 
 
     @Select({"<script> SELECT count(*) " +
-            " FROM chapter c1 JOIN course c2 ON c1.course_id=c2.id where c1.course_id = #{id}"+
+            " FROM chapter c1 JOIN course c2 ON c1.course_id=c2.id where c1.course_id = #{courseId}"+
             "<where>" +
             "<if test='chapterName != null'> and c1.name like '%${chapterName}%' </if> " +
             "</where>" +
             "</script>"})
-    int getRecordCount(String chapterName);
+    int getRecordCount(@Param("chapterName") String chapterName,@Param("courseId") Integer courseId);
 
 
-    //@Insert("insert into chapter() value()")
-    void add(Chapter chapter);
+    @Insert("insert into chapter(name,course_id,info,video) value(#{chapter.name},#{chapter.name},#{chapter.course_id},#{chapter.info},#{chapter.video})")
+    void add(@Param("chapter") Chapter chapter);
 
 
     //
